952ba8bcfbe0228fa753c2c276bdd7b5f7ebb555
bharatpanigrahi12 <bharat.panigrahi@solytics-partners.com>
2026-03-03 11:24:12 +0530

    feat: initial database setup

---
+++ src/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Global, Module } from '@nestjs/common'
+import { neonConfig, Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
+import ws from 'ws'
+
+import * as schema from './schema'
+
+
+export const DRIZZLE_DB = 'DRIZZLE_DB'
+neonConfig.webSocketConstructor = ws
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE_DB,
+      useFactory: () => {
+        const connectionString = process.env.DATABASE_URL
+        if (!connectionString) {
+          throw new Error('DATABASE_URL is not defined')
+        }
+        const pool = new Pool({connectionString})
+        return drizzle(pool, {schema})
+      }
+    }
+  ],
+  exports: [DRIZZLE_DB],
+})
+
+export class DatabaseModule {}
